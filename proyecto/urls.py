2a61f90ba6b5edad6ac66c87f3fe2170f28bfc3f
Jgranados <alexestebann64@gmail.com>
2023-04-22 19:36:55 -0500

    Inventario

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from tasks import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +30,7 @@ urlpatterns = [
     path('choose/', views.choose, name='choose'),
     path('grid/', views.grid, name='grid'),
     path('inventario/', views.inventario, name='inventario')
-]
+]   
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
